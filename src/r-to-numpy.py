e928dd238fd900474cede95430ffc451cceb8b19
Justin Chen <jyc135@ucsd.edu>
2018-02-16 01:05:09 -0800

    added sample data and made the program run properlywith different variable names for given data

--- src/r-to-numpy.py
+++ src/r-to-numpy.py
@@ -5,44 +5,35 @@ import os
 
 usage_message = "Usage: python " + os.path.basename(__file__) + " [file]"
 
-dataName = 'all_classes'
-
 #reads in .Rdata file into a numpy array 
 def readRFileToNumpy(dataFilePath):
 	data_dir = (os.path.abspath(os.path.dirname(dataFilePath)))
 	dataFile = (os.path.basename(dataFilePath))
 
-	r('setwd("' + data_dir + '")')
-	r('load("'+ dataFile +'")')
+	ro.r('setwd("' + data_dir + '")')
+	ro.r('load("'+ dataFile +'")')
 
-	ls = str(r('ls()'))
+	ls = str(ro.r('ls()'))
 	startIndex = str.find(ls, '"') + 1
 	endIndex = str.find(ls, '"', startIndex + 1)
 	dataName = ls[startIndex:endIndex]
 
-	rows = rToInt(r('dim(' + dataName + ')[1]'))
-	cols = rToInt(r('dim(' + dataName + ')[2]'))
-
-	rows = 5;
-	cols = 5;
+	rows = rToInt(ro.r('dim(' + dataName + ')[1]'))
+	cols = rToInt(ro.r('dim(' + dataName + ')[2]'))
 
 	data = np.zeros((rows, cols))
 	
 	for i in range(0, rows): 
 		for j in range(0, cols):
-			data[i, j] = getDataAt(i + 1, j + 1)
+			data[i, j] = getDataAt(dataName, i + 1, j + 1)
 
 	return data
 
 #gets the data point at a certain point in the matrix (in R)
-def getDataAt(i, j):
-	data = r(dataName + '[' + str(i) + ', ' + str(j) + ']')
+def getDataAt(dataName, i, j):
+	data = ro.r(dataName + '[' + str(i) + ', ' + str(j) + ']')
 	return rToInt(data)
 	
-#runs an R command
-def r(command):
-	return ro.r(command)
-
 #converts R output to a String, removing the numbered prefix
 def rToString(rOutput):
 	outputString = str(rOutput)
@@ -59,3 +50,4 @@ if __name__ == "__main__":
 		sys.exit(0)
 	inputFile = sys.argv[1]
 	print(readRFileToNumpy(inputFile))
+
